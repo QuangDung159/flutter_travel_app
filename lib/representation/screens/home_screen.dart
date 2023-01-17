17263112ee3a90dc522720b0f1655602d286ed57
QDLu <quangdunglu159@gmail.com>
2023-01-17 21:41:20 +0700

    Add

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/core/constants/color_constants.dart';
+import 'package:flutter_travel_app/core/constants/dimension_constants.dart';
+import 'package:flutter_travel_app/core/helpers/asset_helper.dart';
+import 'package:flutter_travel_app/core/helpers/image_helper.dart';
 import 'package:flutter_travel_app/representation/widgets/app_bar_container.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,12 +16,51 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return AppBarContainer(
-      implementLeading: true,
-      implementTrailing: true,
-      titleString: 'asdqwd',
-      child: Container(
-        color: Colors.amber
+      title: Padding(
+        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hi James',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: kMediumPadding,
+                ),
+                Text(
+                  'Where are you going next',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+            Spacer(),
+            Icon(
+              Icons.notifications,
+              size: kDefaultIconSize,
+            ),
+            SizedBox(
+              width: kMinPadding,
+            ),
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ImageHelper.loadFromAsset(AssetHelper.imageIntro2),
+            )
+          ],
+        ),
       ),
+      child: Container(color: Colors.amber),
     );
   }
 }

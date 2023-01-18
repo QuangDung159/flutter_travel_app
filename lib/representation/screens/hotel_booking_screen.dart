401681946117397479e34b405a156cfaa45ec317
QDLu <quangdunglu159@gmail.com>
2023-01-18 12:00:28 +0700

    Add

--- lib/representation/screens/hotel_booking_screen.dart
+++ lib/representation/screens/hotel_booking_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_app/core/constants/dimension_constants.dart';
 import 'package:flutter_travel_app/core/helpers/asset_helper.dart';
 import 'package:flutter_travel_app/representation/widgets/app_bar_container.dart';
 import 'package:flutter_travel_app/representation/widgets/item_booking_widget.dart';
@@ -17,41 +18,53 @@ class _HotelBookingScreenState extends State<HotelBookingScreen> {
   Widget build(BuildContext context) {
     return AppBarContainer(
       titleString: 'Hotel booking',
-      child: Column(
-        children: [
-          ItemBookingWidget(
-            icon: SizedBox(
-              width: 12.8,
-              height: 19.2,
-              child: Image.asset(
-                AssetHelper.iconLocation,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: kMediumPadding * 2,
+            ),
+            ItemBookingWidget(
+              icon: SizedBox(
+                width: 12.8,
+                height: 19.2,
+                child: Image.asset(
+                  AssetHelper.iconLocation,
+                ),
               ),
+              color: Color(0xffFE9C5E),
+              title: 'Destination',
+              subTitle: 'South Korea',
+              onTap: () {
+                print('object');
+              },
             ),
-            color: Color(0xffFE9C5E),
-            title: 'Destination',
-            subTitle: 'South Korea',
-          ),
-          ItemBookingWidget(
-            icon: SizedBox(
-              width: 19.2,
-              height: 19.2,
-              child: Image.asset(AssetHelper.iconCalendar),
+            ItemBookingWidget(
+              icon: SizedBox(
+                width: 19.2,
+                height: 19.2,
+                child: Image.asset(
+                  AssetHelper.iconCalendar,
+                ),
+              ),
+              color: Color(0xffF77777),
+              title: 'Select Date',
+              subTitle: '13 Feb - 18 Feb 2021',
             ),
-            color: Color(0xffF77777),
-            title: 'Select Date',
-            subTitle: '13 Feb - 18 Feb 2021',
-          ),
-          ItemBookingWidget(
-            icon: SizedBox(
-              width: 23,
-              height: 20,
-              child: Image.asset(AssetHelper.iconBed),
+            ItemBookingWidget(
+              icon: SizedBox(
+                width: 23,
+                height: 20,
+                child: Image.asset(
+                  AssetHelper.iconBed,
+                ),
+              ),
+              color: Color(0xff3EC8BC),
+              title: 'Guest and Room',
+              subTitle: '2 Guest, 1 Room',
             ),
-            color: Color(0xff3EC8BC),
-            title: 'Guest and Room',
-            subTitle: '2 Guest, 1 Room',
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/representation/widgets/item_booking_widget.dart
+++ lib/representation/widgets/item_booking_widget.dart
@@ -8,60 +8,65 @@ class ItemBookingWidget extends StatelessWidget {
     required this.color,
     required this.title,
     required this.subTitle,
+    this.onTap,
   });
 
   final Widget icon;
   final Color color;
   final String title;
   final String subTitle;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(kDefaultPadding),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: kDefaultBorderRadius,
-          ),
-          child: Row(
-            children: [
-              Container(
-                  alignment: Alignment.center,
-                  width: 32,
-                  height: 32,
-                  decoration: BoxDecoration(
-                      color: color.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(12)),
-                  child: icon),
-              SizedBox(
-                width: kDefaultPadding,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                  ),
-                  SizedBox(
-                    height: 6,
-                  ),
-                  Text(
-                    subTitle,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(kDefaultPadding),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: kDefaultBorderRadius,
+            ),
+            child: Row(
+              children: [
+                Container(
+                    alignment: Alignment.center,
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                        color: color.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: icon),
+                SizedBox(
+                  width: kDefaultPadding,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      subTitle,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: kDefaultPadding,
-        )
-      ],
+          SizedBox(
+            height: kDefaultPadding,
+          )
+        ],
+      ),
     );
   }
 }

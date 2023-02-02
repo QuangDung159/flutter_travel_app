f27fae1095eef5f2855cefd333144c6199780422
QDLu <quangdunglu159@gmail.com>
2023-02-02 20:38:15 +0700

    Add

--- lib/core/constants/common_constant.dart
+++ lib/core/constants/common_constant.dart
@@ -1,2 +1,2 @@
-// const String baseUrl = 'https://lqd-blog-be-nodejs.onrender.com/api/v1';
-const String baseUrl = 'http://localhost:5001/api/v1';
+const String baseUrl = 'https://lqd-blog-be-nodejs.onrender.com/api/v1';
+// const String baseUrl = 'http://localhost:5001/api/v1';
